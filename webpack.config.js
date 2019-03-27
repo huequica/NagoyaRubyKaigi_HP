466d65eedbef58499452e3e69d44fe7799541d0d
Eito Katagiri <eitoball@gmail.com>
2019-03-27 09:24:45 +0900

    add html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,12 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
   mode: process.env.NODE_ENV || 'development',
   output: {
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: 'src/index.html' })
+  ]
 };
