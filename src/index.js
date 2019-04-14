705a95fc14f68d3f7fdc42911f7a264bc0120b57
Eito Katagiri <eitoball@gmail.com>
2019-04-15 01:33:56 +0900

    use mobile-detector.js instead

--- src/index.js
+++ src/index.js
@@ -1,31 +1,19 @@
 require('./main.scss');
-var getDevice = (function(){
-    var ua = navigator.userAgent;
-    if(ua.indexOf('iPhone') > 0 || ua.indexOf('iPod') > 0 || ua.indexOf('Android') > 0 && ua.indexOf('Mobile') > 0){
-        return 'sp';
-    }else if(ua.indexOf('iPad') > 0 || ua.indexOf('Android') > 0){
-        return 'tab';
-    }else{
-        return 'other';
-    }
-})();
 
-if(getDevice == "sp" || getDevice == "tab"){
+const MobileDetect = require('mobile-detect');
 
-  let mobile_br = document.createElement('br')
-  let tags = document.getElementsByClassName('Mobile_br')
-  tags = Array.prototype.slice.call(tags)
-  //配列に強制変換
-  tags.forEach(function(item){
-    //console.log(item);
-    item.insertAdjacentHTML('afterbegin','<br>');
-  })
+import event_logo from './assets/Event_Logo.png'
 
-}
+document.addEventListener('DOMContentLoaded', (event) => {
+  const md = new MobileDetect(navigator.userAgent);
+  if (md.mobile()) {
+    document.querySelectorAll('.Mobile_br').forEach((item) => {
+      item.insertAdjacentHTML('afterbegin', '<br>');
+    })
+  }
 
-import event_logo    from './assets/Event_Logo.png'
+  let event_logo_img = document.createElement('img')
+  event_logo_img.src = event_logo
+  document.getElementById('Event-Logo').appendChild(event_logo_img)
+});
 
-
-let event_logo_img = document.createElement('img')
-event_logo_img.src = event_logo
-document.getElementById('Event-Logo').appendChild(event_logo_img)
