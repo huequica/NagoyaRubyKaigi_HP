1eb0ddff3679c2a6cf7be904eabf12e97bce68b2
huequica <harumaki1225@gmail.com>
2019-04-14 19:49:31 +0900

    イベントロゴを追加、モバイル用に改行を追加

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
 require('./main.scss');
+var getDevice = (function(){
+    var ua = navigator.userAgent;
+    if(ua.indexOf('iPhone') > 0 || ua.indexOf('iPod') > 0 || ua.indexOf('Android') > 0 && ua.indexOf('Mobile') > 0){
+        return 'sp';
+    }else if(ua.indexOf('iPad') > 0 || ua.indexOf('Android') > 0){
+        return 'tab';
+    }else{
+        return 'other';
+    }
+})();
 
+if(getDevice == "sp" || getDevice == "tab"){
 
+  let mobile_br = document.createElement('br')
+  let tags = document.getElementsByClassName('Mobile_br')
+  tags = Array.prototype.slice.call(tags)
+  //配列に強制変換
+  tags.forEach(function(item){
+    //console.log(item);
+    item.insertAdjacentHTML('afterbegin','<br>');
+  })
+
+}
+
+import event_logo    from './assets/Event_Logo.png'
 import huequica_icon from './assets/huequica.png'
 import pepabo        from './assets/pepabo.png'
 import esm           from './assets/ESM.png'
@@ -9,6 +32,11 @@ import ateam         from './assets/ateam.png'
 import stmn          from './assets/stmn.svg'
 import kurusugawa    from './assets/Kurusugawa-Densan.png'
 import sansan        from './assets/sansan.png'
+
+
+let event_logo_img = document.createElement('img')
+event_logo_img.src = event_logo
+document.getElementById('Event-Logo').appendChild(event_logo_img)
 
 //特別講演欄アイコン
 let icon = document.createElement('img')
