5c9e52764ed12938b0d7545b3fdb26498fb7894a
Eito Katagiri <eitoball@gmail.com>
2019-04-15 01:50:19 +0900

    provider images via file-loader

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ require('./main.scss');
 
 const MobileDetect = require('mobile-detect');
 
-import event_logo from './assets/Event_Logo.png'
-
 document.addEventListener('DOMContentLoaded', (event) => {
   const md = new MobileDetect(navigator.userAgent);
   if (md.mobile()) {
@@ -11,9 +9,5 @@ document.addEventListener('DOMContentLoaded', (event) => {
       item.insertAdjacentHTML('afterbegin', '<br>');
     })
   }
-
-  let event_logo_img = document.createElement('img')
-  event_logo_img.src = event_logo
-  document.getElementById('Event-Logo').appendChild(event_logo_img)
 });
 
